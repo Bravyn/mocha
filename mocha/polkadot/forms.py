6451d361e84d464e467bfff1fb25bd7d2e997a79
Ian Bravyn. <ianbravynsa@gmail.com>
2024-04-14 13:38:24 +0300

    .

--- mocha/polkadot/forms.py
+++ mocha/polkadot/forms.py
@@ -4,4 +4,4 @@ from .models import ImageModel
 class ImageForm(forms.ModelForm):
     class Meta:
         model = ImageModel
-        fields = ['image']
+        fields = ['image', 'comments']
--- mocha/polkadot/urls.py
+++ mocha/polkadot/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page, name = "polkhome"),
+    #path('', views.index_page, name = "polkhome"),
     path('polkadot/', views.index_page, name = 'polkadot'),
     path('polkadot/number/<int:num>/', views.number, name='number'),
     path('polkadot/image_upload/', views.upload_image, name= "upload_image"),
